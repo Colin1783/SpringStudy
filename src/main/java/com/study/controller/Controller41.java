f94fcb9a06a49006961b412d5549a403bc242d3b
Colin <hiedra97@gmail.com>
2024-05-16 11:41:14 +0900

    /api/main41/sub3 요청 처리 메서드 작성

---
+++ src/main/java/com/study/controller/Controller41.java
@@ -0,0 +1,31 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/main41")
+public class Controller41 {
+
+	// @RequestBody
+	// 역직렬화 (parsing, decoding) JSON -> Map.
+
+	@PostMapping("/sub1")
+	public void method1(@RequestBody Map<String, Object> map) {
+		System.out.println("map = " + map);
+	}
+
+	@PostMapping("/sub2")
+	public void method2(@RequestBody Map<String, Object> map) {
+		System.out.println("map = " + map);
+	}
+
+	@PostMapping("/sub3")
+	public void method3(@RequestBody Map<String, Object> map) {
+		System.out.println("map = " + map);
+	}
+}
