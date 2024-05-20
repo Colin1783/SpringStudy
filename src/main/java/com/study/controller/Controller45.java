909fc81c630470d428732f06abd7e7d154320a14
Colin <hiedra97@gmail.com>
2024-05-20 16:23:56 +0900

    axixos로 받은 files handler

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -1,16 +1,12 @@
 package com.study.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("/api/main45")
 public class Controller45 {
 	@PostMapping("sub1")
